0f6f6648d47894449667c044b60b312653b3979d
Niwenjin <ni_wenjin@qq.com>
2023-07-25 16:34:37 +0800

    add change value to new value by key

--- include/bpt.h
+++ include/bpt.h
@@ -10,13 +10,19 @@ class BPlusTree
 public:
     BPlusTree(int m) : root(nullptr), _m(m){};
     void insert(string, string);
-    string search(string);
+    bool remove(string);
+    string *search(string);
+    bool change(string, string);
 
 private:
-    const size_t _m;
+    const size_t _m; // b+树的度
     Node *root;
     Node *head; // 第一个叶子节点指针
     void insert(Node *, string, string);
     void split(Node *);
-    string search(Node *, string);
+    bool remove(string);
+    bool update(Node *, string, string);
+    void borrow(Node *, Node *, bool);
+    void merge(Node *, Node *, bool);
+    Node *search(Node *, string);
 };
--- src/bpt.cpp
+++ src/bpt.cpp
@@ -55,10 +55,8 @@ void BPlusTree::insert(Node *p, string key, string value)
     size_t i = 0;
     while (i < p->n && key > p->keys[i])
         i++;
-    auto it = p->keys.begin();
-    p->keys.insert(it + i, key);
-    it = p->values.begin();
-    p->values.insert(it + i, value);
+    p->keys.insert(p->keys.begin() + i, key);
+    p->values.insert(p->values.begin() + i, value);
     p->n++;
     // 节点满，分裂
     if (p->n == _m + 1)
@@ -92,10 +90,8 @@ void BPlusTree::split(Node *p)
         size_t i = 0;
         while (p->keys[t] > p->parent->keys[i])
             i++;
-        auto kit = p->parent->keys.begin();
-        p->parent->keys.insert(kit + i, p->keys[t]);
-        auto cit = p->parent->child.begin();
-        p->parent->child.insert(cit + i, p);
+        p->parent->keys.insert(p->parent->keys.begin() + i, p->keys[t]);
+        p->parent->child.insert(p->parent->child.begin() + i, p);
         p->parent->child[i + 1] = bro;
         p->parent->n++;
     }
@@ -139,24 +135,141 @@ void BPlusTree::split(Node *p)
         split(p->parent);
 }
 
-// 在B+树中根据关键字查询键值对
-string BPlusTree::search(string key)
+// 根据关键字删除键值对
+bool BPlusTree::remove(string key)
 {
-    return search(root, key);
+    Node *p = search(root, key);
+    if (p == nullptr)
+        return false; // 目标关键字不存在
+    size_t i = 0;
+    while (i < p->n && key > p->keys[i])
+        i++;
+    if (i == p->n)
+        return false; // 目标关键字不存在
+    // 删除的是节点中最大值
+    if (i == p->n - 1)
+        update(p->parent, key, p->keys[i - 2]);
+    // 删除键值对
+    p->keys.erase(p->keys.begin() + i);
+    p->keys.erase(p->values.begin() + i);
+    p->n--;
+    size_t t = (_m + 1) / 2;
+    // 节点中关键字数量不足
+    if (p->n < t)
+    {
+        // 寻找兄弟
+        i = 0;
+        while (p->parent->keys[i] < key)
+            i++;
+        Node *bro;
+        if (i == 0)
+        {
+            bro = p->parent->child[i + 1];
+            if (bro->n > t)
+                borrow(p, bro, false);
+            else
+                merge(p, bro, false);
+        }
+        else
+        {
+            bro = p->parent->child[i - 1];
+            if (bro->n > t)
+                borrow(p, bro, true); // 左兄弟有多余
+            else if (i != p->parent->n - 1 && p->parent->child[i + 1]->n > t)
+                borrow(p, p->parent->child[i + 1], false); // 右兄弟有多余
+            else
+                merge(p, bro, true);
+        }
+    }
+    return true;
 }
 
-// 在节点中根据关键字查询键值对
-string BPlusTree::search(Node *node, string key)
+void BPlusTree::borrow(Node *p, Node *bro, bool flag)
 {
+    if (flag)
+    {
+        p->keys.insert(p->keys.begin(), bro->keys[bro->n - 1]);
+        p->values.insert(p->values.begin(), bro->values[bro->n - 1]);
+        update(p->parent, bro->keys[bro->n - 1], bro->keys[bro->n - 2]);
+        bro->keys.pop_back();
+        bro->values.pop_back();
+    }
+    else
+    {
+        p->keys.push_back(bro->keys[0]);
+        p->values.push_back(bro->values[0]);
+        update(p->parent, p->keys[p->n - 1], p->keys[p->n]);
+        bro->keys.erase(bro->keys.begin());
+        bro->values.erase(bro->values.begin());
+    }
+    p->n++;
+    bro->n--;
+}
+
+// 从叶子节点向上合并
+void BPlusTree::merge(Node *p, Node *bro, bool flag)
+{
+    
+}
+
+// 从分支节点向上更新关键字
+void update(Node *p, string key, string newkey)
+{
+    while (p)
+    {
+        size_t i = 0;
+        while (i < p->n && key > p->keys[i])
+            i++;
+        if (p->keys[i] != key)
+            return;
+        p->keys[i] = newkey;
+        p = p->parent;
+    }
+}
+
+// 查询键值对所在的叶子节点
+Node *BPlusTree::search(Node *root, string key)
+{
+    Node *p = root;
+    while (!p->isleaf)
+    {
+        size_t i = 0;
+        while (i < p->n && key > p->keys[i])
+            i++;
+        if (i == p->n)
+            return nullptr; // 关键字超出节点范围
+        p = p->child[i];
+    }
+    return p;
+}
+
+// 在叶子节点中查询键值对
+string *BPlusTree::search(string key)
+{
+    Node *p = search(root, key);
+    if (p == nullptr)
+        return nullptr;
     size_t i = 0;
-    while (i < node->keys.size() && key > node->keys[i])
+    while (i < p->n && key > p->keys[i])
         i++;
-    if (i == node->keys.size())
-        return nullptr; // 关键字超出节点范围
-    if (!node->isleaf)
-        return search(node->child[i], key); // 从孩子节点递归查找
-    else if (node->keys[i] != key)
+    if (p->keys[i] != key)
         return nullptr; // 未找到键值对
     else
-        return node->values[i]; // 找到键值对
+        return &p->values[i]; // 找到键值对
+}
+
+// 更新键值对
+bool BPlusTree::change(string key, string value)
+{
+    Node *p = search(root, key);
+    if (p = nullptr)
+        return false;
+    size_t i = 0;
+    while (i < p->n && key > p->keys[i])
+        i++;
+    if (p->keys[i] != key)
+        return false; // 未找到键值对
+    else
+        p->values[i] = value; // 找到键值对
+    return true;
 }
